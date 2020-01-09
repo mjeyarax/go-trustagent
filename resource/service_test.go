b7f916f0b4b766afca5c9aba4601f136895b50a1
Ullas, Manu <manu.ullas@intel.com>
2020-01-09 13:23:52 +0000

    Fixed unit tests and added some basic negative cases

--- resource/service_test.go
+++ resource/service_test.go
@@ -32,6 +32,8 @@ const (
 	TestPort              = 8450
 	TestJWTVerifyCertPath = "jwtsigncert.pem"
 	TestJWTAuthToken      = "eyJhbGciOiJSUzM4NCIsImtpZCI6IjRhZTYyNmQyMWU4NTg2YmYzNTJlZDQ3NTQwMDY5YjU0ZjA3MGFjNjIiLCJ0eXAiOiJKV1QifQ.eyJyb2xlcyI6W3sic2VydmljZSI6IlRBIiwibmFtZSI6IkhWU1VzZXJQZXJtIn1dLCJwZXJtaXNzaW9ucyI6W3sic2VydmljZSI6IlRBIiwicnVsZXMiOlsiYWlrX2NhOnJldHJpZXZlOioiLCJhaWs6cmV0cmlldmU6KiIsImFwcGxpY2F0aW9uX21lYXN1cmVtZW50OmNyZWF0ZToqIiwiYmluZGluZ19rZXk6cmV0cmlldmU6KiIsImRhYTpyZXRyaWV2ZToqIiwiZGVwbG95X21hbmlmZXN0OmNyZWF0ZToqIiwiZGVwbG95X3RhZzpjcmVhdGU6KiIsImhvc3RfaW5mbzpyZXRyaWV2ZToqIiwicXVvdGU6Y3JlYXRlOioiXX1dLCJleHAiOjE4OTM5MTE4NzEsImlhdCI6MTU3ODU1MTg3MSwiaXNzIjoiQUFTIEpXVCBJc3N1ZXIiLCJzdWIiOiJ0YUhWU1VzZXJQZXJtIn0.Bq94ZzHTa3SUW5W76DQk1SNBrgS9uxINqhfYe--c0jS5F8Gd6PJeTM-HcV4w7sqGqIEhC73khqXQ4O9G7uiB8eMS-HI4pczdyV8zwZtgda8EoDUj9EYjByXktpQTZsEcZwh5NEATAylhqev2ZyeESQNwCAO2o9hWDJmLeYZovHygggOaly5zgWElfAkIvLZnvVyfy2M3aoNWtugpY4V7QZME8kAadwuOgTPAHv87x-nElfb4qIBcOzmuVm9Ktm5cnFD_j9QTMDgLnPtipFVQGsGyUz1OglCojEUGbXUNo2wADHyt3D7T3hgmGQustsiWZamjHdjysS2v4N4ZuKAKIZCuNpDWKdU5DwkY5dFTkdB9D_WA0P5Ot9MdkAiJu_eC9Vg6oI3uoH3uPn1uUGKNo1RGtbmpV4QJASA04UHBY_BTIg0Tu86Aol7ctpbjJZkwxhTkVi4mgOi69dq1N1xmekr_Z3M88P4tZezfm6eeUAfUBQ2r52Up3_RvrhFbOIwm"
+	TestBadJWTAuthToken   = "eyJhbGciOiJSUzM4NCIsImtpZCI6IjRhZTYyNmQyMWU4NTg2YmYzNTJlZDQ3NTQwMDY5YjU0ZjA3MGFjNjIiLCJ0eXAiOiJKV1QifQ.eXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxyJyb2xlcyI6W3sic2VydmljZSI6IlRBIiwibmFtZSI6IkhWU1VzZXJQZXJtIn1dLCJwZXJtaXNzaW9ucyI6W3sic2VydmljZSI6IlRBIiwicnVsZXMiOlsiYWlrX2NhOnJldHJpZXZlOioiLCJhaWs6cmV0cmlldmU6KiIsImFwcGxpY2F0aW9uX21lYXN1cmVtZW50OmNyZWF0ZToqIiwiYmluZGluZ19rZXk6cmV0cmlldmU6KiIsImRhYTpyZXRyaWV2ZToqIiwiZGVwbG95X21hbmlmZXN0OmNyZWF0ZToqIiwiZGVwbG95X3RhZzpjcmVhdGU6KiIsImhvc3RfaW5mbzpyZXRyaWV2ZToqIiwicXVvdGU6Y3JlYXRlOioiXX1dLCJleHAiOjE4OTM5MTE4NzEsImlhdCI6MTU3ODU1MTg3MSwiaXNzIjoiQUFTIEpXVCBJc3N1ZXIiLCJzdWIiOiJ0YUhWU1VzZXJQZXJtIn0.Bq94ZzHTa3SUW5W76DQk1SNBrgS9uxINqhfYe--c0jS5F8Gd6PJeTM-HcV4w7sqGqIEhC73khqXQ4O9G7uiB8eMS-HI4pczdyV8zwZtgda8EoDUj9EYjByXktpQTZsEcZwh5NEATAylhqev2ZyeESQNwCAO2o9hWDJmLeYZovHygggOaly5zgWElfAkIvLZnvVyfy2M3aoNWtugpY4V7QZME8kAadwuOgTPAHv87x-nElfb4qIBcOzmuVm9Ktm5cnFD_j9QTMDgLnPtipFVQGsGyUz1OglCojEUGbXUNo2wADHyt3D7T3hgmGQustsiWZamjHdjysS2v4N4ZuKAKIZCuNpDWKdU5DwkY5dFTkdB9D_WA0P5Ot9MdkAiJu_eC9Vg6oI3uoH3uPn1uUGKNo1RGtbmpV4QJASA04UHBY_BTIg0Tu86Aol7ctpbjJZkwxhTkVi4mgOi69dq1N1xmekr_Z3M88P4tZezfm6eeUAfUBQ2r52Up3_RvrhFbOIwm"
+	EmptyJWTToken         = ""
 )
 
 func CreateTestConfig() *config.TrustAgentConfiguration {
@@ -97,6 +99,7 @@ func mockRetrieveJWTSigningCerts() error {
 	return nil
 }
 
+// TestAssetTagServiceNoExistingTags validates asset tag service without any tags - expect 200 OK
 func TestAssetTagServiceNoExistingTags(t *testing.T) {
 	assert := assert.New(t)
 
@@ -132,6 +135,7 @@ func TestAssetTagServiceNoExistingTags(t *testing.T) {
 	assert.Equal(http.StatusOK, response.StatusCode)
 }
 
+// TestAssetTagServiceExistingTags validates asset tag service with existing tags - expect 200 OK
 func TestAssetTagServiceExistingTags(t *testing.T) {
 	assert := assert.New(t)
 
@@ -165,6 +169,78 @@ func TestAssetTagServiceExistingTags(t *testing.T) {
 	response := recorder.Result()
 	fmt.Printf("StatusCode: %d\n", response.StatusCode)
 	assert.Equal(http.StatusOK, response.StatusCode)
+}
+
+// TestBadToken - invalid JWT-tokens in request result - expect HTTP 401 Unauthorized response
+func TestBadToken(t *testing.T) {
+	assert := assert.New(t)
+
+	mockedTpmProvider := new(tpmprovider.MockedTpmProvider)
+	mockedTpmProvider.On("Close").Return(nil)
+	mockedTpmProvider.On("NvIndexExists", mock.Anything).Return(true, nil)
+	mockedTpmProvider.On("NvRelease", mock.Anything, mock.Anything).Return(nil)
+	mockedTpmProvider.On("NvDefine", mock.Anything, mock.Anything, mock.Anything).Return(nil)
+	mockedTpmProvider.On("NvWrite", mock.Anything, mock.Anything, mock.Anything).Return(nil)
+
+	mockedTpmFactory := tpmprovider.MockedTpmFactory{TpmProvider: mockedTpmProvider}
+
+	trustAgentService, err := CreateTrustAgentService(CreateTestConfig(), mockedTpmFactory)
+	assert.NoError(err)
+
+	// setup TA service to use JWT-based authentication
+	trustAgentService.router = mux.NewRouter()
+	trustAgentService.router.Use(middleware.NewTokenAuth("../test/mockJWTDir", "../test/mockCACertsDir", mockRetrieveJWTSigningCerts, cacheTime))
+	trustAgentService.router.HandleFunc("/v2/tag", errorHandler(requiresPermission(setAssetTag(CreateTestConfig(), mockedTpmFactory), []string{postDeployTagPerm}))).Methods("POST")
+
+	jsonString := `{"tag" : "tHgfRQED1+pYgEZpq3dZC9ONmBCZKdx10LErTZs1k/k=", "hardware_uuid" : "7a569dad-2d82-49e4-9156-069b0065b262"}`
+
+	request, err := http.NewRequest("POST", "/v2/tag", bytes.NewBuffer([]byte(jsonString)))
+	assert.NoError(err)
+
+	request.Header.Set("Content-Type", "application/json")
+	request.Header.Add("Authorization", "Bearer "+TestBadJWTAuthToken)
+
+	recorder := httptest.NewRecorder()
+	trustAgentService.router.ServeHTTP(recorder, request)
+	response := recorder.Result()
+	fmt.Printf("StatusCode: %d\n", response.StatusCode)
+	assert.Equal(http.StatusUnauthorized, response.StatusCode)
+}
+
+// TestEmptyToken - empty JWT-tokens in request - expect HTTP 401 Unauthorized response
+func TestEmptyToken(t *testing.T) {
+	assert := assert.New(t)
+
+	mockedTpmProvider := new(tpmprovider.MockedTpmProvider)
+	mockedTpmProvider.On("Close").Return(nil)
+	mockedTpmProvider.On("NvIndexExists", mock.Anything).Return(true, nil)
+	mockedTpmProvider.On("NvRelease", mock.Anything, mock.Anything).Return(nil)
+	mockedTpmProvider.On("NvDefine", mock.Anything, mock.Anything, mock.Anything).Return(nil)
+	mockedTpmProvider.On("NvWrite", mock.Anything, mock.Anything, mock.Anything).Return(nil)
+
+	mockedTpmFactory := tpmprovider.MockedTpmFactory{TpmProvider: mockedTpmProvider}
+
+	trustAgentService, err := CreateTrustAgentService(CreateTestConfig(), mockedTpmFactory)
+	assert.NoError(err)
+
+	// setup TA service to use JWT-based authentication
+	trustAgentService.router = mux.NewRouter()
+	trustAgentService.router.Use(middleware.NewTokenAuth("../test/mockJWTDir", "../test/mockCACertsDir", mockRetrieveJWTSigningCerts, cacheTime))
+	trustAgentService.router.HandleFunc("/v2/tag", errorHandler(requiresPermission(setAssetTag(CreateTestConfig(), mockedTpmFactory), []string{postDeployTagPerm}))).Methods("POST")
+
+	jsonString := `{"tag" : "tHgfRQED1+pYgEZpq3dZC9ONmBCZKdx10LErTZs1k/k=", "hardware_uuid" : "7a569dad-2d82-49e4-9156-069b0065b262"}`
+
+	request, err := http.NewRequest("POST", "/v2/tag", bytes.NewBuffer([]byte(jsonString)))
+	assert.NoError(err)
+
+	request.Header.Set("Content-Type", "application/json")
+	request.Header.Add("Authorization", "Bearer "+EmptyJWTToken)
+
+	recorder := httptest.NewRecorder()
+	trustAgentService.router.ServeHTTP(recorder, request)
+	response := recorder.Result()
+	fmt.Printf("StatusCode: %d\n", response.StatusCode)
+	assert.Equal(http.StatusUnauthorized, response.StatusCode)
 }
 
 // func TestLocalIpAddress(t *testing.T) {
