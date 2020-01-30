79b027e75880b2152bd79ca2ac3b12c7b97b818d
Thompson, Kent <kent.thompson@intel.com>
2020-01-30 13:44:55 -0800

    Make sure redhat-lsb-core is installed since it is required by platform-info to get the os_name, etc.

--- dist/linux/install.sh
+++ dist/linux/install.sh
@@ -80,7 +80,7 @@ TRUSTAGENT_BIN_DIR=$TRUSTAGENT_HOME/bin
 TRUSTAGENT_LOG_DIR=/var/log/trustagent
 TRUSTAGENT_CFG_DIR=$TRUSTAGENT_HOME/configuration
 TRUSTAGENT_VAR_DIR=$TRUSTAGENT_HOME/var/
-TRUSTAGENT_YUM_PACKAGES="tpm2-tss-2.0.0-4.el8.x86_64 tpm2-abrmd-2.1.1-3.el8.x86_64 dmidecode compat-openssl10 logrotate"
+TRUSTAGENT_YUM_PACKAGES="tpm2-tss-2.0.0-4.el8.x86_64 tpm2-abrmd-2.1.1-3.el8.x86_64 dmidecode compat-openssl10 logrotate redhat-lsb-core"
 TBOOT_DEPENDENCY="tboot-1.9.7"
 TPM2_ABRMD_SERVICE=tpm2-abrmd.service
 
